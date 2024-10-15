1b35ba7406305db6482db2eb1f03c63a0c305b71
Piran Tata <2687506+pirahawk@users.noreply.github.com>
2024-10-15 14:00:20 +0100

    Add ability to send Cloud Event message

--- src/DurableTasksLab.Client/Program.cs
+++ src/DurableTasksLab.Client/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Reflection;
+using System.Text.Json;
 using Azure.Identity;
+using Azure.Messaging;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
 
@@ -15,13 +17,31 @@ await using ServiceBusClient client = new(sbNamespace, new DefaultAzureCredentia
 
 ServiceBusSender sender = client.CreateSender(topicName);
 
+var invokeOperationId = Guid.NewGuid();
+var contentType = "application/json";
+
+// For samples on using the CloudEvent schema refer to: https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/servicebus/Azure.Messaging.ServiceBus/samples/Sample11_CloudEvents.md
+
+dynamic cloudEventPayload = new {
+    Id = invokeOperationId,
+    Message = "Sample Message"
+};
+var cloudEvent = new CloudEvent("durabletask-client", "test-durabletask-invoke", cloudEventPayload);
+cloudEvent.Subject = $"operation-{invokeOperationId}";
+
+cloudEvent.ExtensionAttributes.Add("contenttype", contentType);
+
 // create a message that we can send. UTF-8 encoding is used when providing a string.
-ServiceBusMessage message = new("Hello world!");
+ServiceBusMessage message = new(JsonSerializer.Serialize(cloudEvent)){
+    ContentType = contentType,
+    MessageId = $"{invokeOperationId}",
+    Subject = cloudEvent.Subject
+};
 
 // send the message
 await sender.SendMessageAsync(message);
 
-Console.WriteLine($"Hello, World! {configuration["ServiceBus:Namespace"]}");
+Console.WriteLine($"Sent messages to: {configuration["ServiceBus:Namespace"]}");
 
 
 
